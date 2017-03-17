5210df88258db7b487591afafb029ab7d5edd511
zuzanatoldyova <zuzana.toldyova@gmail.com>
2017-03-17 23:41:38 +0000

    added comment with link to preview

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,7 @@
 class UserMailerPreview < ActionMailer::Preview
+
+  # http://localhost:3000/rails/mailers/user_mailer/order_confirmation_preview
+
   def order_confirmation_preview
     UserMailer.order_confirmation(User.first, Order.first)
   end

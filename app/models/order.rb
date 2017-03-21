ac28fe178510b00ae62039e2c53e1dd5352a1b87
zuzanatoldyova <zuzana.toldyova@gmail.com>
2017-03-21 16:11:57 +0000

    decrementing quantity after creation of order

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,15 @@ class Order < ActiveRecord::Base
 
   validates :stripe_charge_id, presence: true
 
+  after_create :decrease_products_quantity
+
+
+  def decrease_products_quantity
+    @items = self.line_items
+    @items.each do |item|
+      new_quantity = item.product.quantity - item.quantity
+      item.product.update(quantity: new_quantity)
+    end
+  end
+
 end
---
+++ spec/models/order_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Order, type: :model do
+  describe 'After creation' do
+
+    before :each do
+      # Setup at least two products with different quantities, names, etc
+      @category = Category.create(name: "kitchen")
+      @product1 = @category.products.create!(name: "chair", price_cents: 150, quantity: 30)
+      @product2 = @category.products.create!(name: "table", price_cents: 250, quantity: 10)
+      # Setup at least one product that will NOT be in the order
+      @product3 = @category.products.create!(name: "teapot", price_cents: 50, quantity: 5)
+
+    end
+    # pending test 1
+    it 'deducts quantity from products based on their line item quantities' do
+      # TODO: Implement based on hints below
+      # 1. initialize order with necessary fields (see orders_controllers, schema and model definition for what is required)
+      @order = Order.new(email: "zuzana@gmail.com", total_cents: 4000, stripe_charge_id: "someid")
+      # 2. build line items on @order
+      @order.line_items.new(product: @product1, quantity: 1, item_price: @product1.price_cents, total_price: @product1.price_cents)
+      @order.line_items.new(product: @product2, quantity: 1, item_price: @product2.price_cents, total_price: @product2.price_cents)
+
+      # 3. save! the order - ie raise an exception if it fails (not expected)
+      @order.save!
+      # 4. reload products to have their updated quantities
+      @product1.reload
+      @product2.reload
+      # 5. use RSpec expect syntax to assert their new quantity values
+     expect(@product1.quantity).to eq(29)
+    end
+    # pending test 2
+    xit 'does not deduct quantity from products that are not in the order' do
+      # TODO: Implement based on hints in previous test
+    end
+  end
+end

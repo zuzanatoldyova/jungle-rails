4103698b07e05da0dca2b90184d1d03767b0b770
zuzanatoldyova <zuzana.toldyova@gmail.com>
2017-03-21 20:29:15 +0000

    testing that user can navigate to product page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see selected product" do
+    visit root_path
+    find('article.product', match: :first).find('header').click
+    expect(page).to have_css 'section.products-show', count: 1
+    save_screenshot
+  end
+
+end

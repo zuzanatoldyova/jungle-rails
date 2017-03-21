2eeb02f7b19bcdd9e41ad85e498bb4e7535023ec
zuzanatoldyova <zuzana.toldyova@gmail.com>
2017-03-21 00:18:12 +0000

    fix of variable name

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if user = User.authenticate_with_credentials(params[:email], params[:password])
+    if @user = User.authenticate_with_credentials(params[:email], params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
       session[:user_id] = @user.id
